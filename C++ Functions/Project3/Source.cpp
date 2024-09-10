8b68a92bf7892378b8e5611d173faa5ef86e9eef
nuranaismayilzada <bd6nyw4qq@code.edu.az>
2024-09-10 14:15:05 +0400

    Functions added

---
+++ C++ Functions/Project3/Source.cpp
@@ -0,0 +1,275 @@
+﻿#include<iostream>
+using namespace std;
+
+
+//1. Ədədin kubunu qaytaran funksiya yazın.
+int Function_Cube_Number(int number) {
+	int cube = number * number * number;
+	return cube;
+}
+
+
+//2. İki ədəddən böyüyünü tapan funksiya yazın.
+double Finds_Larger_Numbers(double number1, double number2) {
+	if (number1 > number2)
+	{
+		return number1;
+	}
+	else if (number1 < number2)
+	{
+		return number2;
+	}
+}
+
+//3. Ədəd müsbətdirsə doğru, mənfidirsə yanlış qaytaran funksiya yazın.
+bool Find_Number_Positive_And_Negative(int num) {
+	if (num > 0)
+	{
+		return true;
+	}
+	else if (num < 0) {
+
+		return false;
+	}
+}
+
+//4. İstifadəçinin seçimindən asılı olaraq Toplama, Çıxma, Vurma, Bölmə funksiyalarından birini çağıran, funksiya yazın.
+double Addition(double number1, double number2) {
+	return number1 + number2;
+}
+double Multiply(double number1, double number2) {
+	return number1 * number2;
+}
+double Subtraction(double number1, double number2) {
+	return number1 - number2;
+}
+double Division(double number1, double number2) {
+	return number1 / number2;
+}
+
+void Choice_Function() {
+	int num1, num2;
+	int choice;
+	cout << "1-Toplama  2-Cixma  3-Vurma  4-Bolme" << endl;
+	cout << "Seciminizi daxil edin:";
+	cin >> choice;
+	cout << "Ededi daxil edin:";
+	cin >> num1;
+	cout << "Ededi daxil edin:";
+	cin >> num2;
+
+	if (choice == 1)
+	{
+		cout << Addition(num1, num2);
+	}
+	else if (choice == 2) {
+		cout << Subtraction(num1, num2);
+	}
+	else if (choice == 3) {
+		cout << Multiply(num1, num2);
+	}
+	else if (choice == 4) {
+		cout << Division(num1, num2);
+	}
+	else {
+		cout << "Bele bir secim yoxdur.";
+	}
+
+}
+
+
+//5. Parametr kimi tərfinin uzunluğunu qəbul edən, və ekrana həmin ölçüdə kvadrat çıxaran funksiya yazın.
+void Create_Square(int size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		for (size_t k = 0; k < size; k++)
+		{
+			if (k == 0 || i == size - 1 || i == 0 || k == size - 1)
+			{
+				cout << "*  ";
+			}
+			else {
+				cout << "   ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+
+//6. Göndərilən rəqəmin sadə olub olmadığını yoxlayan funksiya yazın.
+void Check_Number_Simple(int number) {
+	int bolen = 1;
+	int Count = 0;
+	while (number >= bolen)
+	{
+		if (number % bolen == 0)
+		{
+			Count++;
+		}
+		bolen++;
+	}
+	if (Count == 2)
+	{
+		cout << "Eded sadedir.";
+	}
+	else {
+		cout << "Eded murekkebdir.";
+	}
+}
+
+//7. Ədədin faktorialını qaytaran funksiya yazın.
+
+int Find_Number_Factorial(int num) {
+	int factorial = 1;
+	while (num > 0)
+	{
+		factorial *= num;
+		num--;
+	}
+	return factorial;
+}
+
+//8. İki parametr qəbul edən : Üst və Qüvvət, və ədədin qüvvətini qaytaran funksiya yazın.
+
+int Power_Find_Number(int number, int power) {
+	int result = 1;
+	while (power > 0)
+	{
+		result = result * number;
+		power--;
+	}
+	return result;
+}
+
+//9. Funksiya 2 ədəd qəbul edir və bunlar arasındakı bütün ədədləri toplayıb qaytarır.
+
+double Numbers_Between_Sum(double number1, double number2) {
+	int sum = 0;
+	if (number1 < number2)
+	{
+		while (number1 < number2)
+		{
+			sum += number1;
+			number1++;
+		}
+	}
+	else if (number1 > number2) {
+		while (number1 > number2)
+		{
+			sum += number1;
+			number1++;
+		}
+	}
+	return sum;
+}
+
+//10. Massivdəki ən böyük ədədi qaytaran funksiya yazın.
+
+void Init_Array(int arr[], int size) {
+	int min = 1, max = 100;
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = min + rand() % (max - min + 1);
+	}
+}
+
+void Show_Array(int arr[], int size) {
+	cout << "Massiv:";
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+int Find_Biggest_Number(int arr[],int size) {
+	cout << "Massivdeki en boyuk eded:";
+	int maximumNumber = arr[0];
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i]>maximumNumber)
+		{
+			maximumNumber = arr[i];
+		}
+	}
+	return maximumNumber;
+}
+void main() {
+
+	srand(time(NULL));
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//1. Ədədin kubunu qaytaran funksiya yazın.
+
+		//cout << Function_Cube_Number(6) << endl << endl;
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//2. İki ədəddən böyüyünü tapan funksiya yazın.
+
+		//cout << Finds_Larger_Numbers(10.8, 9.9) << endl<<endl;
+
+//-------------------------------------------------------------------------------------------------------------------------------
+ 
+		//3. Ədəd müsbətdirsə doğru, mənfidirsə yanlış qaytaran funksiya yazın.
+
+		//cout << Find_Number_Positive_And_Negative(5)<<endl;
+
+//-------------------------------------------------------------------------------------------------------------------------------
+ 
+		//4. İstifadəçinin seçimindən asılı olaraq Toplama, Çıxma, Vurma, Bölmə funksiyalarından birini çağıran, funksiya yazın.
+
+		//Choice_Function();
+		//cout << endl;
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//5. Parametr kimi tərfinin uzunluğunu qəbul edən, və ekrana həmin ölçüdə kvadrat çıxaran funksiya yazın.
+
+		//Create_Square(10);
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//6. Göndərilən rəqəmin sadə olub olmadığını yoxlayan funksiya yazın.
+
+		//Check_Number_Simple(9);
+		//cout << endl;
+		//Check_Number_Simple(7);
+
+//-------------------------------------------------------------------------------------------------------------------------------
+ 
+		//7.Ədədin faktorialını qaytaran funksiya yazın.
+
+		//cout << Find_Number_Factorial(4);
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//8. İki parametr qəbul edən : Üst və Qüvvət, və ədədin qüvvətini qaytaran funksiya yazın.
+
+		//cout<<Power_Find_Number(3, 3)<<endl;
+		//cout << Power_Find_Number(5, 3);
+		
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//9. Funksiya 2 ədəd qəbul edir və bunlar arasındakı bütün ədədləri toplayıb qaytarır.
+		
+		//cout<<Numbers_Between_Sum(5, 10);
+		
+//-------------------------------------------------------------------------------------------------------------------------------
+
+		//10. Massivdəki ən böyük ədədi qaytaran funksiya yazın.
+
+		//const int size = 10;
+		//int arr[size]{};
+		//Show_Array(arr, size);
+		//Init_Array(arr, size);
+		//Show_Array(arr, size);
+		//cout << Find_Biggest_Number(arr, size);
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
+}
---
+++ Multi dimensional static array/Project2/Source.cpp
@@ -0,0 +1,485 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+void main() {
+	//1. 5x5 ölçüsündə ikiölçülü massivi 0 - 20 aralığında random ədədlərlə
+	//	doldurun.Massivdəki ədədlərin cəmini tapın.
+	//srand(time(NULL));
+	//int min = 0, max = 20;
+	//const int size = 5;
+	//int arr[size][size]{};
+	//int sum = 0, random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//		sum += arr[i][k];
+	//	}
+	//}
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout << setw(4) << arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//cout << "Massivdeki ededlerin cemi:" << sum;
+
+
+//----------------------------------------------------------------------------------------------------------------
+	//2. 5x5 ölçüsündə ikiölçülü massivi 0 - 20 aralığında random ədədlərlə
+	//	doldurun.Əsas və əks dioqanalda yerləşən rəqəmlərin cəmini
+	//	hesablayan program yazın.
+
+	//srand(time(NULL));
+	//int min = 0, max = 20;
+	//const int size = 5;
+	//int arr[size][size]{};
+	//int sum = 0, random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//	}
+	//}
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		if (i == k || i + k == size - 1)
+	//		{
+	//			sum += arr[i][k];
+	//		}
+	//	}
+	//}
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout << setw(4) << arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//cout << "Massivdeki esas ve eks dioqanaldaki ededlerin cemi:" << sum;
+
+
+//----------------------------------------------------------------------------------------------------------------
+	//3. 5x5 ölçüsündə ikiölçülü massivi(-100) və(100) aralığında random
+	//	ədədlərlə doldurun.Massivdəki minimal və maksimal elementləri
+	//	tapan program yazın.
+
+	//srand(time(NULL));
+	//int min = -100, max = 100;
+	//const int size = 5;
+	//int arr[size][size]{};
+	//int random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//	}
+	//}
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout << setw(4) << arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+
+	//int minNumber = arr[0][0];
+	//int maxNumber = arr[0][0];
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		if (arr[i][k]>maxNumber)
+	//		{
+	//			maxNumber = arr[i][k];
+	//		}
+	//	}
+	//}
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		if (arr[i][k] < minNumber)
+	//		{
+	//			minNumber = arr[i][k];
+	//		}
+	//	}
+	//}
+
+	//cout << endl;
+	//cout << "Massivdeki maximal eded: " << maxNumber<<endl;
+	//cout << "Massivdeki minimal eded: " << minNumber << endl;
+
+//----------------------------------------------------------------------------------------------------------------
+	//4. 5x5 ölçüsündə ikiölçülü massivi - 5 və 5 aralığında random ədədlərlə
+	//doldurun.Massivdəki mənfi, müsbət, və sıfır olan elementlərin sayını
+	//tapın.
+
+	//srand(time(NULL));
+	//int min = -5, max = 5;
+	//const int size = 5;
+	//int minusNumbersCount = 0, pilusNumbersCount = 0, zeroNumbersCount = 0;
+	//int arr[size][size]{};
+	//int random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//	}
+	//}
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout<< setw(3)<<arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		if (arr[i][k]>0)
+	//		{
+	//			pilusNumbersCount++;
+	//		}
+	//		else if (arr[i][k] < 0) {
+	//			minusNumbersCount++;
+	//		}
+	//		else {
+	//			zeroNumbersCount++;
+	//		}
+	//	}
+	//}
+	//cout << endl;
+	//cout << "Massivdeki menfi ededlerin sayi:" << minusNumbersCount << endl;
+	//cout << "Massivdeki musbet ededlerin sayi:" << pilusNumbersCount << endl;
+	//cout << "Massivdeki sifirlarin sayi:" << zeroNumbersCount;
+
+
+//----------------------------------------------------------------------------------------------------------------
+	//5. 5x5 ölçüsündə ikiölçülü massivi 0 - 20 aralığında random ədədlərlə
+	//doldurun.Hər bir sətirdəki elementlərin cəmini tapın.
+
+
+	//srand(time(NULL));
+	//int min = 0, max = 20;
+	//const int size = 5;
+	//int arr[size][size]{};
+	//int random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//	}
+	//}
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout << setw(5) << arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//int sum = 0;
+	//int sumofNumbersArr[size][1]{};
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	sum = 0;
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		sum += arr[i][k];
+	//	}
+	//	sumofNumbersArr[i][0] = sum;
+	//	cout << i << "-ci setirdeki elementlerin cemi:" << sum << endl;
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//cout << "Cemden alinan massiv:" << endl;
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < 1; k++)
+	//	{
+	//		cout << setw(10) << sumofNumbersArr[i][k] << " ";
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+
+//----------------------------------------------------------------------------------------------------------------
+
+		//6. 5x5 ölçüsündə ikiölçülü massivi 0 - 20 aralığında random ədədlərlə
+		//doldurun.Hər bir sütundakı elementlərin cəmini tapın.
+
+		//srand(time(NULL));
+		//int min = 0, max = 20;
+		//const int size = 5;
+		//int arr[size][size]{};
+		//int random;
+		//for (size_t i = 0; i < size; i++)
+		//{
+		//	for (size_t k = 0; k < size; k++)
+		//	{
+		//		random = min + rand() % (max - min + 1);
+		//		arr[i][k] = random;
+		//	}
+		//}
+
+		//for (size_t i = 0; i < size; i++)
+		//{
+		//	for (size_t k = 0; k < size; k++)
+		//	{
+		//		cout << setw(3) << arr[i][k];
+		//	}
+		//	cout << endl;
+		//}
+		//cout << endl;
+		//int sum = 0;
+		//int sumofNumbersArr[size]{};
+		//for (size_t i = 0; i < size; i++)
+		//{
+		//	sum = 0;
+		//	for (size_t k = 0; k < size; k++)
+		//	{
+		//		sum += arr[k][i];
+		//	}
+		//	sumofNumbersArr[i] = sum;
+		//	cout << i << "-ci setirdeki elementlerin cemi:" << sum << endl;
+		//	cout << endl;
+		//}
+		//cout << endl;
+		//cout << "Cemden alinan massiv:" << endl;
+		//for (size_t i = 0; i < size; i++)
+		//{
+		//	cout << sumofNumbersArr[i] << " ";
+		//}
+		//cout << endl;
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+	//7. 5x5 ölçüsündə ikiölçülü massivi - 10 və 40 aralığında random
+	//ədədlərlə doldurun.Bütün sətirdə mənfi ədəd yoxdursa, bu sətirin
+	//cəmini toplayın.
+
+
+	//srand(time(NULL));
+	//int min = -10, max = 40;
+	//const int size = 5;
+	//int arr[size][size]{};
+	//int random;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		random = min + rand() % (max - min + 1);
+	//		arr[i][k] = random;
+	//	}
+	//}
+
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		cout << setw(3) << arr[i][k];
+	//	}
+	//	cout << endl;
+	//}
+	//cout << endl;
+	//int sum = 0;
+	//int sum_2 = 0;
+	//int newSumArr[size]{};
+	//bool isCheck = false;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	sum = 0;
+	//	isCheck = false;
+	//	for (size_t k = 0; k < size; k++)
+	//	{
+	//		if (arr[k][i] > 0)
+	//		{
+	//			continue;
+	//		}
+	//		else {
+	//			isCheck = true;
+	//		}
+	//	}
+	//	for (size_t j = 0; j < size; j++)
+	//	{
+	//		if (isCheck == false)
+	//		{
+	//			sum += arr[j][i];
+	//		}
+	//	}
+	//	if (isCheck == false)
+	//	{
+	//		newSumArr[i] = sum;
+	//		cout << i << "-ci setirde menfi eded yoxdur ve cemi " << sum << " -a beraberdir." << endl;
+	//	}
+	//}
+	//cout << "[";
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	cout<< newSumArr[i] << ",";
+	//}
+	//cout << "]";
+
+//--------------------------------------------------------------------------------------------------------------------
+	//8. N x M ölçülü massivi elə ikirəqəmli ədədlərlə doldurun ki, birinci
+	//rəqəm sətirin, ikinci rəqəm sütun nömrəsini göstərsin.
+
+	//const int sizen = 7;
+	//const int sizem = 9;
+	//int arr[sizen][sizem]{};
+
+	//for (size_t i = 0; i < sizen; i++)
+	//{
+	//	for (size_t k = 0; k < sizem; k++)
+	//	{
+	//		arr[i][k] = (i * 10 + k);
+	//	}
+	//}
+	//for (size_t i = 0; i < sizen; i++)
+	//{
+	//	for (size_t k = 0; k < sizem; k++)
+	//	{
+	//		if (arr[i][k] < 10)
+	//		{
+	//			cout << "0" << arr[i][k] << "  ";
+	//		}
+	//		else {
+	//			cout << arr[i][k] << "  ";
+	//		}
+	//	}
+	//	cout << endl;
+	//}
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+			//9. N xM ölçülü massivdə, birinci və sonuncu sütunun, ikinci və sondan
+			//əvvəlki sütunun və s.yerini dəyişin.
+
+			//srand(time(NULL));
+			//int min = 10, max = 99;
+			//const int sizem = 4;
+			//const int sizen = 6;
+			//int arr[sizem][sizen]{};
+			//int random, temp;
+			//cout << "   Massiv  " << endl<<endl;
+			//for (size_t i = 0; i < sizem; i++)
+			//{
+			//	for (size_t k = 0; k < sizen; k++)
+			//	{
+			//		random = min + rand() % (max - min + 1);
+			//		arr[i][k] = random;
+			//	}
+			//}
+
+			//for (size_t i = 0; i < sizem; i++)
+			//{
+			//	for (size_t k = 0; k < sizen; k++)
+			//	{
+			//		cout << setw(3) << arr[i][k];
+			//	}
+			//	cout << endl;
+			//}
+			//cout << endl;
+
+			//for (size_t i = 0; i < sizem; i++)
+			//{
+			//	for (size_t k = 0; k < sizen/2; k++)
+			//	{
+			//		temp = arr[i][k];
+			//		arr[i][k] = arr[i][sizen -1- k];
+			//		arr[i][sizen -k-1] = temp;
+			//	}
+			//}
+			//cout << endl;
+			//cout << " Cevrilmis massiv   "<<endl;
+			//cout << endl;
+			//for (size_t i = 0; i < sizem; i++)
+			//{
+			//	for (size_t k = 0; k < sizen; k++)
+			//	{
+			//		cout << setw(3) << arr[i][k];
+			//	}
+			//	cout << endl;
+			//}
+
+//----------------------------------------------------------------------------------------------------------------
+
+						//10. 6 x 6 ölçülü massivdə, qonşu sətirlərin yerini dəyişin.
+
+						//srand(time(NULL));
+						//int min = 1, max = 100;
+						//const int size = 6;
+						//int arr[size][size]{};
+						//int random;
+						//for (size_t i = 0; i < size; i++)
+						//{
+						//	for (size_t k = 0; k < size; k++)
+						//	{
+						//		random = min + rand() % (max - min + 1);
+						//		arr[i][k] = random;
+						//	}
+						//}
+
+						//cout << "Massiv:" << endl;
+						//for (size_t i = 0; i < size; i++)
+						//{
+						//	for (size_t k = 0; k < size; k++)
+						//	{
+						//		cout << setw(3) << arr[i][k];
+						//	}
+						//	cout << endl;
+						//}
+						//cout << endl;
+						//cout << "Qonsu setirlerin yeri deyisilmis massiv:" << endl;
+						//int temp;
+						//for (size_t i = 0; i < size; i++)
+						//{
+						//	for (size_t k = 0; k < size; k++)
+						//	{
+						//		temp = arr[i * 2][k];
+						//		arr[i * 2][k] = arr[i * 2 + 1][k];
+						//		arr[i * 2 + 1][k] = temp;
+						//	}
+						//}
+						//for (size_t i = 0; i < size; i++)
+						//{
+						//	for (size_t k = 0; k < size; k++)
+						//	{
+						//		cout << setw(3) << arr[i][k];
+						//	}
+						//	cout << endl;
+						//}
+
+//----------------------------------------------------------------------------------------------------------------
+}
